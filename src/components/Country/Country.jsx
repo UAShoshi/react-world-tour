b793661cf6a6ce23f8cca77a5cd87557a12239e7
UAShoshi <ummeaymonafifha@gmail.com>
2024-03-12 15:07:23 +0600

    first commit

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,13 @@
+import './Country.css'
+const Country = ({country}) => {
+  const {name, flags} = country;
+  return (
+    <div className='Country'>
+      <h3>Name: {name?.common}</h3>
+      <img src={flags.png} alt="" />
+      
+    </div>
+  );
+};
+
+export default Country;
